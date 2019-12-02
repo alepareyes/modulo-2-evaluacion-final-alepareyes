dee2aa8a3c3d8a70895d6d0438b21328a05a3469
alepareyes <alejandra.reyesg96@gmail.com>
2019-12-02 15:05:02 +0100

    add fav shows

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,9 +4,10 @@ console.log('>> Ready :)');
 
 
 let showsContainer = document.querySelector('.js-shows-container');
+let favShowsContainer = document.querySelector('.js-favshows-container');
 
 let shows = [];
-const favoritesShows = [];
+let favoritesShows = [];
 
 const btn = document.querySelector('.js-btn');
 
@@ -24,6 +25,8 @@ function getServerData() {
             shows = data;
             console.log(shows);
             paintShows();
+            listenShows();
+
         })
         .catch(function (err) {
             console.log("Error al traer los datos del servidor", err);
@@ -35,11 +38,12 @@ function paintShows() {
 
     let htmlCode = '';
 
-
     for (let i = 0; i < shows.length; i++) {
+
         const imgDefault = `https://via.placeholder.com/210x295/ffffff/666666/?text=TV`;
 
-        htmlCode += `<div class="shows__item js-shows-item" id="${i}">`;
+
+        htmlCode += `<div class="shows__item js-shows-item" id="${shows[i].show.id}">`;
         htmlCode += `<h2 class="shows__name">${shows[i].show.name}</h2>`;
 
         if (shows[i].show.image === null) {
@@ -49,15 +53,70 @@ function paintShows() {
             htmlCode += `<img class="shows__photo" src="${img}" />`;
         }
 
-        htmlCode += `Géneros: ${shows[i].show.genres}<br>`;
+        htmlCode += `Género: ${shows[i].show.genres}<br>`;
         htmlCode += `Fecha: ${shows[i].show.premiered}`;
         htmlCode += `</div>`;
     }
-
     showsContainer.innerHTML = htmlCode;
 }
 
 
-btn.addEventListener('click', getServerData);
+function paintFavShows() {
 
-getServerData();
+    let htmlCode = '';
+
+    for (let i = 0; i < favoritesShows.length; i++) {
+
+        const imgDefault = `https://via.placeholder.com/210x295/ffffff/666666/?text=TV`;
+
+        htmlCode += `<div class="shows__item js-shows-item" id="${favoritesShows[i].show.id}">`;
+        htmlCode += `<h2 class="shows__name">${favoritesShows[i].show.name}</h2>`;
+
+        if (favoritesShows[i].show.image === null) {
+            htmlCode += `<img class="shows__photo" src="${imgDefault}" />`
+        } else {
+            const img = favoritesShows[i].show.image.medium;
+            htmlCode += `<img class="shows__photo" src="${img}" />`;
+        }
+
+        htmlCode += `Género: ${favoritesShows[i].show.genres}<br>`;
+        htmlCode += `Fecha: ${favoritesShows[i].show.premiered}`;
+        htmlCode += `</div>`;
+    }
+    favShowsContainer.innerHTML = htmlCode;
+}
+
+
+
+function addToFavorites(ev) {
+
+    const clickedId = parseInt(ev.currentTarget.id);
+
+    for (let i = 0; i < shows.length; i++) {
+        if (clickedId === shows[i].show.id) {
+            favoritesShows.push(shows[i]);
+        } else {
+        }
+    }
+    console.log(clickedId, favoritesShows);
+
+    paintFavShows();
+}
+
+
+function listenShows() {
+    const showsItems = document.querySelectorAll(".js-shows-item");
+    for (const showsItem of showsItems) {
+        showsItem.addEventListener('click', addToFavorites);
+    }
+}
+
+
+function forSearch(ev) {
+    ev.preventDefault();
+    getServerData();
+}
+
+
+btn.addEventListener('click', forSearch);
+
