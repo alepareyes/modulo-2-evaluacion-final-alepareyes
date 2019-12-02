56209126f7cdf20cda3058c179834c50dc38b1bd
alepareyes <alejandra.reyesg96@gmail.com>
2019-12-02 10:38:35 +0100

    add search bar & paint results on screen

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,53 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+
+let showsContainer = document.querySelector('.js-shows-container');
+
+let shows = [];
+const favoritesShows = [];
+
+const btn = document.querySelector('.js-btn');
+
+
+
+function getServerData() {
+
+    const search = document.querySelector('.js-input').value;
+
+    let url = `http://api.tvmaze.com/search/shows?q=${search}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            shows = data;
+            console.log(shows);
+            paintShows();
+        })
+        .catch(function (err) {
+            console.log("Error al traer los datos del servidor", err);
+        });
+}
+
+
+function paintShows() {
+
+    let htmlCode = '';
+
+    for (let i = 0; i < shows.length; i++) {
+        htmlCode += `<div class="shows__item js-shows-item" id="${i}">`;
+        htmlCode += `<h2 class="shows__name">${shows[i].show.name}</h2>`;
+        htmlCode += `<img class="shows__photo" src="${shows[i].show.image}" />`;
+        htmlCode += `Géneros: ${shows[i].show.genders}<br>`;
+        htmlCode += `Fecha: ${shows[i].show.premiered}`;
+        htmlCode += `</div>`;
+    }
+
+    showsContainer.innerHTML = htmlCode;
+}
+
+
+btn.addEventListener('click', getServerData);
+
+getServerData();
